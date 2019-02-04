ac372f0a7e53d31273c5dd88d9563347fde71773
Lena Schnedlitz <lena.schnedlitz@mail.de>
2019-02-04 20:57:28 +0100

    Add venue service

---
+++ luupsmap/service/venue_service.py
@@ -0,0 +1,11 @@
+from model import db
+from model.venue import Venue
+
+
+class VenueService:
+    def __init__(self):
+        self.db_session = db.session
+        self.entity = Venue
+
+    def find_all(self):
+        return self.db_session.query(self.entity).all()
