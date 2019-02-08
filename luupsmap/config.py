379c38b172fb54fb4dcd4f69dbe41eb526a37f6c
Lena Schnedlitz <lena.schnedlitz@mail.de>
2019-02-08 09:50:33 +0100

    Restructure config file

--- luupsmap/config.py
+++ luupsmap/config.py
@@ -6,11 +6,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = os.getenv('SECRET_KEY')
+    GMAPS_API_KEY = os.getenv('GMAPS_API_KEY')
 
     # DB
     DB_USER = os.getenv('DB_USER')
     DB_PW = os.getenv('DB_PW')
-    DB_NAME = os.getenv('DB_NAME')
+    DB_NAME = None
     SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{pw}@{address}/{db}'.format(
         user=DB_USER,
         pw=DB_PW,
@@ -18,18 +19,22 @@ class Config(object):
         db=DB_NAME
     )
 
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
+    # DEBUG,...
+    DEBUG = False
+    DEVELOPMENT = False
     TESTING = False
 
 
+class DevelopmentConfig(Config):
+    DB_NAME = 'luups_map_dev'
+    DEBUG = True
+    DEVELOPMENT = True
+
+
 class TestingConfig(Config):
-    DEBUG = False
+    DB_NAME = 'luups_map_test'
     TESTING = True
 
 
 class ProductionConfig(Config):
-    DEBUG = False
-    TESTING = False
+    DB_NAME = 'luups_map'
