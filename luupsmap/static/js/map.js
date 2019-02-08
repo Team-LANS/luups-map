133ab2dc90856b8592532929aa2c5c1b2c86fa95
Lena Schnedlitz <lena.schnedlitz@mail.de>
2019-02-08 23:36:22 +0100

    Implement marker types

--- luupsmap/static/js/map.js
+++ luupsmap/static/js/map.js
@@ -1,3 +1,9 @@
+const VENUE_TYPES = {
+  RESTAURANT: 'RESTAURANT',
+  BAR: 'BAR',
+  VENUE: 'VENUE'
+};
+
 const VIENNA_COORDS = {
   lat: 48.208176,
   lng: 16.373819
@@ -11,16 +17,27 @@ let map;  // the map object
 
 getMapContainer = () => document.getElementById('map');
 
+getMarkerIcon = (type) => {
+  return {
+    url: `${location.href}static/svg/marker/${VENUE_TYPES[type]}.svg`,
+    scaledSize: new google.maps.Size(45, 45)
+  }
+};
+
+addMarker = (type, position) => {
+  return new google.maps.Marker({
+    map,
+    position,
+    animation: google.maps.Animation.DROP,
+    icon: getMarkerIcon(type)
+  });
+};
+
 initMap = () => {
   const options = {
     center: VIENNA_COORDS,
     zoom: DEFAULT_ZOOM
   };
   map = new google.maps.Map(getMapContainer(), options);
-  const marker = new google.maps.Marker({
-    map,
-    position: VIENNA_COORDS,
-    title: 'Wien!',
-    animation: google.maps.Animation.DROP
-  });
+  const marker = addMarker('VENUE', VIENNA_COORDS);
 };
