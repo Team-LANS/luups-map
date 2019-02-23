52db5f009373af63617fedabb01d9328a8110b98
hschne <hans.schnedlitz@gmail.com>
2019-02-23 15:18:26 +0100

    Add venue detail fetching

--- luupsmap/cli/commands/__init__.py
+++ luupsmap/cli/commands/__init__.py
@@ -1,3 +1,2 @@
 from luupsmap.cli.commands.seed import *
-from luupsmap.cli.commands.update_locations import *
-from luupsmap.cli.commands.update_addresses import *
+from luupsmap.cli.commands.update import *
