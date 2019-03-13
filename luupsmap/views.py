126266f28830410cd12e74e083bdf233536c0da1
hschne <hans.schnedlitz@gmail.com>
2019-03-13 21:15:36 +0100

    Add initial form and popup. It sucks though :imp:

--- luupsmap/views.py
+++ luupsmap/views.py
@@ -1,15 +1,19 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, flash, url_for
 
 from luupsmap import app, Venue
+from luupsmap.forms import LoginForm
 from luupsmap.service import VenueService
 
 venue_service = VenueService()
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     venues = venue_service.find_all()
-    return render_template('main.jinja2', venues=venues)
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index', _anchor="none"))
+    return render_template('main.jinja2', venues=venues, form=form)
 
 
 @app.route('/venues')
