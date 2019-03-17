abae5998cdbb757076f40d405b4259165adc0aeb
hschne <hans.schnedlitz@gmail.com>
2019-03-17 22:13:21 +0100

    Add tags

--- luupsmap/views.py
+++ luupsmap/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request
 
 from luupsmap import app
 from luupsmap.service import VenueService
-from luupsmap.model import Type
+from luupsmap.model import Type, Tag
 
 venue_service = VenueService()
 
@@ -10,7 +10,9 @@ venue_service = VenueService()
 @app.route('/', methods=['GET'])
 def index():
     venues_ = venue_service.find_all()
-    return render_template('main.html', venues=venues_)
+    tags = [t.name.replace("_", " ").lower() for t in Tag]
+    types = [t.name.lower() for t in Type]
+    return render_template('main.html', venues=venues_, types=types, tags=tags)
 
 
 @app.route('/', methods=['POST'])
@@ -20,13 +22,19 @@ def filter():
         types.append(Type.FOOD)
     if "drinks" in request.form.keys():
         types.append(Type.DRINK)
-    if "entertainment" in request.form.keys():
+    if "ticket" in request.form.keys():
         types.append(Type.TICKET)
 
     venues_ = venue_service.find_by_type(types)
 
     return render_template('main.html', venues=venues_)
 
+
 @app.route('/<int:venue_id>', methods=['GET'])
 def show_details(venue_id):
     return render_template('details.html', venue=venue_service.get(venue_id))
+
+
+def pretty_tag(tag):
+    tag = tag.replace("_", " ")
+
