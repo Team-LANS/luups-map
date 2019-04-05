ec718a0c4976df54e32189d25e89dc903406e22b
hschne <hans.schnedlitz@gmail.com>
2019-04-05 09:53:54 +0200

    Make details display infos for current venue

--- luupsmap/views.py
+++ luupsmap/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, redirect, url_for, request
 
 from luupsmap import app
-from luupsmap.service import VenueService
+from luupsmap.service import VenueService, LocationService
 from luupsmap.model import Type, Tag
 
 venue_service = VenueService()
+location_service = LocationService()
 
 
 @app.route('/', methods=['GET'])
@@ -26,7 +27,7 @@ def filter():
     return render_template('main.html', venues=venues_, types=types, tags=tags)
 
 
-@app.route('/<int:venue_id>', methods=['GET'])
-def show_details(venue_id):
-    return render_template('details.html', venue=venue_service.get(venue_id))
-
+@app.route('/<int:location_id>', methods=['GET'])
+def show_details(location_id):
+    location = location_service.get(location_id)
+    return render_template('details.html', venue=venue_service.get(location.id_venue), location=location)
