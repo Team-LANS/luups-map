6a8ef96d6fabc78f0eea1051be1b29d0d3424bab
hschne <hans.schnedlitz@gmail.com>
2019-07-06 18:39:59 +0200

    Add cli documentation

--- luupsmap/cli/data.py
+++ luupsmap/cli/data.py
@@ -14,6 +14,13 @@ data = AppGroup('data')
 @data.command('seed')
 @with_appcontext
 def seed():
+    """
+    Resets the database, and seeds new data from CSV files
+
+    Usage:
+
+        flask data seed
+    """
     remove_data()
     path = os.path.abspath(os.path.dirname(__file__))
     venues_file = os.path.join(path, '..', '..', 'data', 'venues.csv')
@@ -21,30 +28,50 @@ def seed():
     vouchers_file = os.path.join(path, '..', '..', 'data', 'vouchers.csv')
     SeedCommand(venues_file, locations_file, vouchers_file).run()
 
+
 @data.command('reset')
 @with_appcontext
 def reset():
+    """
+    Removes all data from the database, but leaves columns as is.
+
+    Usage:
+
+        flask data reset
+    """
     remove_data()
 
 
 @data.command('nuke')
 @with_appcontext
 def reset():
+    """
+    Resets the database, dropping all tables.
+
+    Usage:
+
+        flask data nuke
+    """
     nuke()
 
 
 @data.command('update')
 @with_appcontext
 def update():
+    """
+    Updates the CSV documents available for data seeding by fetching data from the Google API.
+
+    Usage:
+
+        flask data nuke
+    """
     path = os.path.abspath(os.path.dirname(__file__))
     locations = os.path.join(path, '..', '..', 'data', 'locations.csv')
     venues = os.path.join(path, '..', '..', 'data', 'venues.csv')
     UpdateCommand(locations_file=locations, venues_file=venues).run()
 
 
-# noinspection SqlWithoutWhere
 def remove_data():
-    # TODO: Use logger
     print('Removing existing data...'.ljust(LINE_LENGTH), end=' ')
     session = db.session
     session.query(VoucherTag).delete()
